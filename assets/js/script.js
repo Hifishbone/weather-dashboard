c9aa8c0b7ba7072e326cb960b11189636d744e83
Jerry <jerrywei998@gmail.com>
2021-11-28 01:23:05 -0500

    Finished: 5-Day Forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,6 @@ function saveToHistory(city) {
 function updateSearchHistoryView() {
     // <button type="button" class="btn btn-secondary my-2 w-100">Search</button>
     $('#history-buttons').html("");
-    console.log(searchHistory);
     for (let i = 0; i < searchHistory.length; i++) {
         var city = searchHistory[i];
         $('#history-buttons').append(`<button type="button" class="btn btn-secondary text-capitalize my-2 w-100" id="history-btn-${i}">${city}</button>`);
@@ -47,8 +46,8 @@ var getWeatherData = function (city) {
             alert("Error: " + response.statusText);
         }
     }).catch(function (error) {
-            alert("Unable to connect to openweathermap");
-        });
+        alert("Unable to connect to openweathermap");
+    });
 };
 
 var getWeatherDataByLatLon = function (lat, lon, city) {
@@ -72,10 +71,12 @@ var getWeatherDataByLatLon = function (lat, lon, city) {
 
 function updateInfoView(data, city) {
     updateCurDayView(data["current"], city);
+    update5DayView(data["daily"]);
 }
 
 
 function updateCurDayView(data, city) {
+    console.log(data);
     var curDay = moment().format('l');
     $('#city-date').text(city + ' (' + curDay + ')');
     var iconUrl = `http://openweathermap.org/img/wn/${data["weather"][0]["icon"]}.png`;
@@ -96,6 +97,27 @@ function updateCurDayView(data, city) {
         $('#cur-uv').attr("style", "background-color:rgb(198,42,34)");
     } else {
         $('#cur-uv').attr("style", "background-color:rgb(102,74,193)");
+    }
+}
+
+
+function update5DayView(data) {
+    $("#five-days").html("");
+    for (let i = 1; i < data.length && i < 6; i++) {
+        var day = data[i];
+        var iconUrl = `http://openweathermap.org/img/wn/${day["weather"][0]["icon"]}.png`;
+        var card =
+            `<div class="col"><div class="card h-100 w-100">
+        <div class="card-body five-day-card">
+            <h5 class="card-title">${moment().add(i, 'days').format('l')}</h5>
+            <img class="" src="${iconUrl}" alt="${day["weather"][0]["description"]}">
+            <div class="my-2"><span>Temp: </span><span>${day["temp"]["eve"]}\u2109</span></div>
+            <div class="my-2"><span>Wind: </span><span>${day["wind_speed"]} MPH</span></div>
+            <div class="my-2"><span>Humidity: </span><span>${day["humidity"]} %</span></div>
+            <div>
+        </div>
+        </div></div>`
+        $("#five-days").append(card);
     }
 }
 
